544a341fe6725b1da067e55059ac51b742831039
micha <mimagra@gmx.net>
2024-03-05 21:11:39 +0100

    started to implement Multiplayer functions (not finished)

--- src/main/java/de/dhbw/ase/play/games/Multiplayer.java
+++ src/main/java/de/dhbw/ase/play/games/Multiplayer.java
@@ -1,7 +1,15 @@
 package de.dhbw.ase.play.games;
 
+import de.dhbw.ase.play.games.multiplayer.MultiplayerGame;
+
 import java.util.Scanner;
 
-public class Multiplayer {
+public class Multiplayer extends MultiplayerGame {
 
+
+    public Multiplayer(Scanner sc) {
+        super(sc, null, null);
+        //TODO
+
+    }
 }
--- src/main/java/de/dhbw/ase/play/games/MultiplayerQuick.java
+++ src/main/java/de/dhbw/ase/play/games/MultiplayerQuick.java
@@ -1,7 +1,15 @@
 package de.dhbw.ase.play.games;
 
+import de.dhbw.ase.play.games.multiplayer.MultiplayerGame;
+
 import java.util.Scanner;
 
-public class MultiplayerQuick {
+public class MultiplayerQuick extends MultiplayerGame {
 
+    public MultiplayerQuick(Scanner sc) {
+        super(sc, null, null);
+
+        // TODO
+
+    }
 }
