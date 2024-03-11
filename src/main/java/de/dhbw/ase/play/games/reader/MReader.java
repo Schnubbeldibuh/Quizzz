439278b05811552320254687ed4e09340a0d50bc
Gloria <gloria.landsberger@gmail.com>
2024-03-11 21:04:53 +0100

    Implemented temporary multiplayer reader

--- src/main/java/de/dhbw/ase/play/games/reader/MReader.java
+++ src/main/java/de/dhbw/ase/play/games/reader/MReader.java
@@ -1,4 +1,16 @@
 package de.dhbw.ase.play.games.reader;
 
-public class MReader {
+import de.dhbw.ase.Quizzz;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MReader extends Reader{
+    @Override
+    public List<Question> getQuestionList() {
+        List<Question> questionsForOneRound = new ArrayList<>(readFile(new File(Quizzz.FILE_WWM_EASY), 5));
+        //TODO neuen Datensatz mit Fragen für Mulitplayer erstellen und hier anpassen
+        return questionsForOneRound;
+    }
 }
