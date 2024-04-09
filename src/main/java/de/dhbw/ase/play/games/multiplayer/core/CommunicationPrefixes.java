15f7ee92ae0b63684c0f05f755e048718898bd6b
micha <mimagra@gmx.net>
2024-04-09 13:36:38 +0200

    introduced Enum for Communication Strings

---
+++ src/main/java/de/dhbw/ase/play/games/multiplayer/core/CommunicationPrefixes.java
@@ -0,0 +1,29 @@
+package de.dhbw.ase.play.games.multiplayer.core;
+
+public enum CommunicationPrefixes {
+    USERNAME("username:"),
+    DUPLIKATE_USERNAME("duplicate username"),
+    SUCCESSFULLY_JOINED("successfully_joined"),
+    NEXT_QUESTION("next_question:"),
+    ANSWER("answer:"),
+    START_GAME("startgame"),
+    ROUND_FINISHED("round_finished");
+
+    private final String string;
+
+    CommunicationPrefixes(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public boolean checkPrefix(String input) {
+        return input.startsWith(string);
+    }
+
+    public int getLength() {
+        return string.length();
+    }
+}
