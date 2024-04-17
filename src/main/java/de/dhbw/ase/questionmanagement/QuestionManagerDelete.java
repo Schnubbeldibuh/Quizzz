9b45a69a74013fc651b983930fc8d2bf9da6128c
micha <mimagra@gmx.net>
2024-04-18 00:10:35 +0200

    fixed deleting wrong question

--- src/main/java/de/dhbw/ase/questionmanagement/QuestionManagerDelete.java
+++ src/main/java/de/dhbw/ase/questionmanagement/QuestionManagerDelete.java
@@ -1,40 +1,37 @@
 package de.dhbw.ase.questionmanagement;
 
 import de.dhbw.ase.SelectedMenu;
-import de.dhbw.ase.Startable;
 
 import java.util.Scanner;
 
 public class QuestionManagerDelete extends QuetionEditor {
 
-    private Scanner sc;
-    private QuestionManagerEditingMenu questionManagerEditingMenu;
+    private final QuestionManagerEditingMenu questionManagerEditingMenu;
 
     public QuestionManagerDelete(Scanner sc, QuestionManagerEditingMenu questionManagerEditingMenu) {
-        this.sc = sc;
         this.questionManagerEditingMenu = questionManagerEditingMenu;
     }
 
     @Override
     public SelectedMenu.MenuSelection start() {
-        QuestionObject questionObject = null;
+        //QuestionObject questionObject = null;
         readFileContent(questionManagerEditingMenu.getFilePath());
         getQuestionIndex(questionManagerEditingMenu.getSelectedLine());
-        for (int i = 0; i < file.size(); i++) {
+        /*for (int i = 0; i < file.size(); i++) {
             questionObject = file.get(i);
             System.out.println(questionObject);
             if (Integer.parseInt(questionObject.getQuestionIndex()) == (questionIndex-1)) {
                 break;
             }
         }
-        int questionIndex = file.indexOf(questionObject);
-        file.remove(questionIndex);
+        int questionIndex = file.indexOf(questionObject);*/
+        file.remove(this.questionIndex);
 
-        for (int i = 0; i < questionIndex; i++) {
+        for (int i = 0; i < this.questionIndex; i++) {
             int currentQuestionIndex = Integer.parseInt(file.get(i).getQuestionIndex());
             file.get(i).setQuestionIndex(String.valueOf(currentQuestionIndex - 1));
         }
-        questionObject.setQuestionIndex(file.size() + "");
+        //questionObject.setQuestionIndex(file.size() + "");
         writeBackToFile(questionManagerEditingMenu.getFilePath());
         return SelectedMenu.MenuSelection.BACK;
     }
