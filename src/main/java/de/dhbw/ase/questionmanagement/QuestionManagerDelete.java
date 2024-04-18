595326dfe3d2cc20ee32c272dc0a5be4210144bd
Gloria <gloria.landsberger@gmail.com>
2024-04-18 12:07:47 +0200

    Deleted comments

--- src/main/java/de/dhbw/ase/questionmanagement/QuestionManagerDelete.java
+++ src/main/java/de/dhbw/ase/questionmanagement/QuestionManagerDelete.java
@@ -14,24 +14,14 @@ public class QuestionManagerDelete extends QuetionEditor {
 
     @Override
     public SelectedMenu.MenuSelection start() {
-        //QuestionObject questionObject = null;
         readFileContent(questionManagerEditingMenu.getFilePath());
         getQuestionIndex(questionManagerEditingMenu.getSelectedLine());
-        /*for (int i = 0; i < file.size(); i++) {
-            questionObject = file.get(i);
-            System.out.println(questionObject);
-            if (Integer.parseInt(questionObject.getQuestionIndex()) == (questionIndex-1)) {
-                break;
-            }
-        }
-        int questionIndex = file.indexOf(questionObject);*/
         file.remove(this.questionIndex);
 
         for (int i = 0; i < this.questionIndex; i++) {
             int currentQuestionIndex = Integer.parseInt(file.get(i).getQuestionIndex());
             file.get(i).setQuestionIndex(String.valueOf(currentQuestionIndex - 1));
         }
-        //questionObject.setQuestionIndex(file.size() + "");
         writeBackToFile(questionManagerEditingMenu.getFilePath());
         return SelectedMenu.MenuSelection.BACK;
     }
