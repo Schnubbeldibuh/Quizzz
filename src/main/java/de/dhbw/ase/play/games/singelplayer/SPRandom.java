c143a6fb27c0cdd00322a4ddf9d1a3c97b950dbc
Gloria <gloria.landsberger@gmail.com>
2024-04-23 18:05:03 +0200

    Implemented random category singleplayer mode

--- src/main/java/de/dhbw/ase/play/games/singelplayer/SPRandom.java
+++ src/main/java/de/dhbw/ase/play/games/singelplayer/SPRandom.java
@@ -2,24 +2,34 @@ package de.dhbw.ase.play.games.singelplayer;
 
 import de.dhbw.ase.user.in.UserIn;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class SPRandom extends SingleplayerGame {
 
-    public SPRandom(UserIn sc) {
+    private final List<SPQuiz> spGamesList;
+    private SPQuiz randomGame;
+
+    public SPRandom(UserIn sc, List<SPQuiz> spGamesList) {
         super(sc);
+        this.spGamesList = spGamesList;
     }
 
     @Override
     protected void startGame() {
-
+        Collections.shuffle(spGamesList);
+        randomGame = spGamesList.get(0);
+        randomGame.startGame();
     }
 
     @Override
     protected String getStatsFilesPath() {
-        return null;
+        return randomGame.getStatsFilesPath();
     }
 
     @Override
     protected void writeStats() {
-
+        randomGame.writeStats();
     }
 }
--- src/main/java/de/dhbw/ase/play/games/singelplayer/SingleplayerCategoryMenu.java
+++ src/main/java/de/dhbw/ase/play/games/singelplayer/SingleplayerCategoryMenu.java
@@ -5,7 +5,9 @@ import de.dhbw.ase.SelectedMenu;
 import de.dhbw.ase.Submenu;
 import de.dhbw.ase.user.in.UserIn;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SingleplayerCategoryMenu extends Submenu {
@@ -17,20 +19,33 @@ public class SingleplayerCategoryMenu extends Submenu {
     protected Map<String, SelectedMenu> createSelectionMap() {
 
         Map<String, SelectedMenu> map = new HashMap<>();
-        map.put("1", new SelectedMenu(new SPRandom(getSc())));
-        map.put("2", new SelectedMenu(
-                new SPQuiz(getSc(), Quizzz.FILE_SP_GENERAL, Categories.GENERAL, Quizzz.FILE_STATS_SP_GENERAL)));
-        map.put("3", new SelectedMenu(
-                new SPQuiz(getSc(), Quizzz.FILE_SP_GEOGRAPHY, Categories.GEOGRAPHY, Quizzz.FILE_STATS_SP_GEOGRAPHY)));
-        map.put("4", new SelectedMenu(
-                new SPQuiz(getSc(), Quizzz.FILE_SP_CINEMA_TV, Categories.CINEMA_TV, Quizzz.FILE_STATS_SP_CINEMA_TV)));
-        map.put("5", new SelectedMenu(
-                new SPQuiz(getSc(),
-                        Quizzz.FILE_SP_TECHNOLOGY, Categories.TECHNOLOGY_INTERNET, Quizzz.FILE_STATS_SP_TECHNOLOGY)));
-        map.put("6", new SelectedMenu(
-                new SPQuiz(getSc(), Quizzz.FILE_SP_SPORTS, Categories.SPORTS, Quizzz.FILE_STATS_SP_SPORTS)));
-        map.put("7", new SelectedMenu(
-                new SPQuiz(getSc(), Quizzz.FILE_SP_FOOD, Categories.FOOD, Quizzz.FILE_STATS_SP_FOOD)));
+
+        SPQuiz generalQuiz = new SPQuiz(getSc(),
+                Quizzz.FILE_SP_GENERAL, Categories.GENERAL, Quizzz.FILE_STATS_SP_GENERAL);
+        SPQuiz geographyQuiz = new SPQuiz(getSc(),
+                Quizzz.FILE_SP_GEOGRAPHY, Categories.GEOGRAPHY, Quizzz.FILE_STATS_SP_GEOGRAPHY);
+        SPQuiz cinemaTVQuiz = new SPQuiz(getSc(),
+                Quizzz.FILE_SP_CINEMA_TV, Categories.CINEMA_TV, Quizzz.FILE_STATS_SP_CINEMA_TV);
+        SPQuiz technologyInternetQuiz = new SPQuiz(getSc(),
+                Quizzz.FILE_SP_TECHNOLOGY, Categories.TECHNOLOGY_INTERNET, Quizzz.FILE_STATS_SP_TECHNOLOGY);
+        SPQuiz sportsQuiz = new SPQuiz(getSc(), Quizzz.FILE_SP_SPORTS, Categories.SPORTS, Quizzz.FILE_STATS_SP_SPORTS);
+        SPQuiz foodQuiz = new SPQuiz(getSc(), Quizzz.FILE_SP_FOOD, Categories.FOOD, Quizzz.FILE_STATS_SP_FOOD);
+
+        List<SPQuiz> spGamesList = new ArrayList<>();
+        spGamesList.add(geographyQuiz);
+        spGamesList.add(generalQuiz);
+        spGamesList.add(cinemaTVQuiz);
+        spGamesList.add(technologyInternetQuiz);
+        spGamesList.add(sportsQuiz);
+        spGamesList.add(foodQuiz);
+
+        map.put("1", new SelectedMenu(new SPRandom(getSc(), spGamesList)));
+        map.put("2", new SelectedMenu(generalQuiz));
+        map.put("3", new SelectedMenu(geographyQuiz));
+        map.put("4", new SelectedMenu(cinemaTVQuiz));
+        map.put("5", new SelectedMenu(technologyInternetQuiz));
+        map.put("6", new SelectedMenu(sportsQuiz));
+        map.put("7", new SelectedMenu(foodQuiz));
         map.put("8", new SelectedMenu(SelectedMenu.MenuSelection.BACK));
         map.put("9", new SelectedMenu(SelectedMenu.MenuSelection.EXIT));
 
