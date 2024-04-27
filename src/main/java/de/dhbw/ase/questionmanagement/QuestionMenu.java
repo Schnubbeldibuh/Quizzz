56782a0d7311c3550321ee8b1a62ff32cc4fd5d8
Gloria <gloria.landsberger@gmail.com>
2024-04-27 09:49:43 +0200

    Added missing options to question menu

--- src/main/java/de/dhbw/ase/questionmanagement/QuestionMenu.java
+++ src/main/java/de/dhbw/ase/questionmanagement/QuestionMenu.java
@@ -20,13 +20,27 @@ public class QuestionMenu extends Submenu {
     @Override
     protected Map<String, SelectedMenu> generateSelectionMap() {
         Map<String, SelectedMenu> map = new HashMap<>();
-        map.put("1", new SelectedMenu(new WWMQuestionMenu(sc)));
+        map.put("1", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_MP))));
         map.put("2", new SelectedMenu(
                 new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_MP))));
-        map.put("3", new SelectedMenu(
+        map.put("3", new SelectedMenu(new WWMQuestionMenu(sc)));
+        map.put("4", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_SP_GEOGRAPHY))));
+        map.put("5", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_SP_CINEMA_TV))));
+        map.put("6", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_SP_GENERAL))));
+        map.put("7", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_SP_TECHNOLOGY))));
+        map.put("8", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_SP_SPORTS))));
+        map.put("9", new SelectedMenu(
+                new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_SP_FOOD))));
+        map.put("10", new SelectedMenu(
                 new QuestionManager(sc, QuestionRepositoryFilebased.getInstance(Quizzz.FILE_FQ2))));
-        map.put("4", new SelectedMenu(SelectedMenu.MenuSelection.BACK));
-        map.put("5", new SelectedMenu(SelectedMenu.MenuSelection.EXIT));
+        map.put("11", new SelectedMenu(SelectedMenu.MenuSelection.BACK));
+        map.put("12", new SelectedMenu(SelectedMenu.MenuSelection.EXIT));
 
         return map;
     }
@@ -34,10 +48,17 @@ public class QuestionMenu extends Submenu {
     @Override
     protected void showOptions() {
         System.out.println("Welchen Fragenkatalog möchtest du bearbeiten?");
-        System.out.println("1 - Wer wird Milionär?");
-        System.out.println("2 - Multiplayer");
-        System.out.println("3 - Fragenfinder");
-        System.out.println("4 - Zurück");
-        System.out.println("5 - Exit");
+        System.out.println("1 - Multiplayer");
+        System.out.println("2 - Multiplayer - Schnellmodus");
+        System.out.println("3 - Wer wird Milionär?");
+        System.out.println("4 - Kategorie Geographie");
+        System.out.println("5 - Kategorie Film und Fernsehen");
+        System.out.println("6 - Kategorie Allgemeinwissen");
+        System.out.println("7 - Kategorie Technologie und Internet");
+        System.out.println("8 - Kategorie Sport");
+        System.out.println("9 - Kategorie Essen und Trinken");
+        System.out.println("10 - Fragenfinder");
+        System.out.println("11 - Zurück");
+        System.out.println("12 - Exit");
     }
 }
