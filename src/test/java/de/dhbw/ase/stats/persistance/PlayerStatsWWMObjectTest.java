eb8f8713885a16be8bf7c98367c2e0a46dddbea0
Gloria <gloria.landsberger@gmail.com>
2024-04-29 21:18:05 +0200

    Tested to string and compare to of WWM stats object

--- src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsWWMObjectTest.java
+++ src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsWWMObjectTest.java
@@ -2,8 +2,14 @@ package de.dhbw.ase.stats.persistance;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PlayerStatsWWMObjectTest {
 
@@ -17,6 +23,10 @@ class PlayerStatsWWMObjectTest {
 
     @Test
     void testToString() {
+        PlayerStatsWWMObject playerStatsWWMObject1 = new PlayerStatsWWMObject("test", 1, 2, 3);
+
+        String toString = playerStatsWWMObject1.toString();
+        Assertions.assertEquals("test;1;2;3", toString);
     }
 
     @Test
@@ -37,19 +47,25 @@ class PlayerStatsWWMObjectTest {
         Assertions.assertEquals(1, playerStatsWWMObject2.getPoints());
     }
 
-    @Test
-    void compareTo() {
+    @ParameterizedTest
+    @MethodSource("compareToValues")
+    void compareTo(int points1, int points2,
+                   int money1, int money2,
+                   int rightAnswers1, int rightAnswers2, int expected) {
+        PlayerStatsWWMObject playerStatsWWMObject1 = new PlayerStatsWWMObject("test", points1, money1, rightAnswers1);
+        PlayerStatsWWMObject playerStatsWWMObject2 = new PlayerStatsWWMObject("test", points2, money2, rightAnswers2);
+
+        int i = playerStatsWWMObject1.compareTo(playerStatsWWMObject2);
+        Assertions.assertEquals(expected, i);
     }
 
-    @Test
-    void getRightAnswers() {
-    }
-
-    @Test
-    void getPoints() {
-    }
-
-    @Test
-    void getMoney() {
+    private static Stream<Arguments> compareToValues() {
+        return Stream.of(
+                arguments(2, 3, 2, 3, 2, 3, -1),
+                arguments(3, 2, 2, 3, 2, 3, 1),
+                arguments(2, 2, 2, 3, 2, 3, -1),
+                arguments(2, 2, 3, 2, 2, 3, 1),
+                arguments(2, 2, 4, 4, 1, 1, 0)
+        );
     }
 }
