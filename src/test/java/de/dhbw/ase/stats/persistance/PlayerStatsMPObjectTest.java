fb5aea0b15481f5fd9d88872a84fda938458a88c
micha <mimagra@gmx.net>
2024-04-29 18:33:21 +0200

    implementing compareTo test for PlayerStatsMPObjectTest

--- src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsMPObjectTest.java
+++ src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsMPObjectTest.java
@@ -2,8 +2,25 @@ package de.dhbw.ase.stats.persistance;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PlayerStatsMPObjectTest {
+
+    private static Stream<Arguments> compareToValues() {
+        return Stream.of(
+                arguments(1, 2, 2, 3, 1, 1, -1),
+                arguments(3, 2, 2, 3, 1, 1, 1),
+                arguments(2, 2, 2, 3, 1, 1, 1),
+                arguments(2, 2, 4, 3, 1, 1, -1),
+                arguments(2, 2, 4, 4, 1, 1, 0)
+                );
+    }
 
     @Test
     void fromLine() {
@@ -39,19 +56,13 @@ class PlayerStatsMPObjectTest {
         Assertions.assertEquals(1, playerStatsMPObject2.getPoints());
     }
 
-    @Test
-    void compareTo() {
-    }
+    @ParameterizedTest
+    @MethodSource("compareToValues")
+    void compareTo(int rightAnswer1, int rightAnswer2, int wrongAnswer1, int wrongAnswer2, int points1, int points2, int expected) {
+        PlayerStatsMPObject playerStatsMPObject1 = new PlayerStatsMPObject("test", rightAnswer1, wrongAnswer1, points1);
+        PlayerStatsMPObject playerStatsMPObject2 = new PlayerStatsMPObject("test", rightAnswer2, wrongAnswer2, points2);
 
-    @Test
-    void getRightAnswers() {
-    }
-
-    @Test
-    void getWrongAnswers() {
-    }
-
-    @Test
-    void getPoints() {
+        int i = playerStatsMPObject1.compareTo(playerStatsMPObject2);
+        Assertions.assertEquals(expected, i);
     }
 }
