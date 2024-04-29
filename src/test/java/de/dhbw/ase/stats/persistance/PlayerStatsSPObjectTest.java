cc51883200e87aff287d7f57fc9a2bdaec7c0095
Gloria <gloria.landsberger@gmail.com>
2024-04-29 19:13:30 +0200

    Tested to string and compare to of SP stats object

--- src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsSPObjectTest.java
+++ src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsSPObjectTest.java
@@ -2,8 +2,14 @@ package de.dhbw.ase.stats.persistance;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PlayerStatsSPObjectTest {
 
@@ -34,22 +40,32 @@ class PlayerStatsSPObjectTest {
     }
 
     @Test
-    void compareTo() {
-    }
-
-    @Test
     void testToString() {
+        PlayerStatsSPObject playerStatsSPObject1 = new PlayerStatsSPObject("test", 1, 2, 3);
+
+        String toString = playerStatsSPObject1.toString();
+        Assertions.assertEquals("test;1;2;3", toString);
     }
 
-    @Test
-    void getRightAnswers() {
+    @ParameterizedTest
+    @MethodSource("compareToValues")
+    void compareTo(int rightAnswer1, int rightAnswer2, int wrongAnswer1, int wrongAnswer2, int averageAnswerTime1, int averageAnswerTime2, int expected) {
+        PlayerStatsSPObject playerStatsSPObject1 = new PlayerStatsSPObject("test", rightAnswer1, wrongAnswer1, averageAnswerTime1);
+        PlayerStatsSPObject playerStatsSPObject2 = new PlayerStatsSPObject("test", rightAnswer2, wrongAnswer2, averageAnswerTime2);
+
+        int i = playerStatsSPObject1.compareTo(playerStatsSPObject2);
+        Assertions.assertEquals(expected, i);
     }
 
-    @Test
-    void getWrongAnswers() {
-    }
-
-    @Test
-    void getAverageAnswerTime() {
+    private static Stream<Arguments> compareToValues() {
+        return Stream.of(
+                arguments(1, 2, 2, 3, 1, 1, -1),
+                arguments(3, 2, 2, 3, 1, 1, 1),
+                arguments(2, 2, 2, 3, 1, 1, 1),
+                arguments(2, 2, 4, 3, 1, 1, -1),
+                arguments(2, 2, 3, 3, 1, 2, 1),
+                arguments(2, 2, 3, 3, 2, 1, -1),
+                arguments(2, 2, 4, 4, 1, 1, 0)
+        );
     }
 }
