97b1d2d387ef532389acb1d6988b9c5609a5713e
Gloria <gloria.landsberger@gmail.com>
2024-04-29 15:24:44 +0200

    Implemented phone joker for wwm

--- src/main/java/de/dhbw/ase/play/games/singelplayer/wwm/PhoneJoker.java
+++ src/main/java/de/dhbw/ase/play/games/singelplayer/wwm/PhoneJoker.java
@@ -10,12 +10,30 @@ import java.util.Set;
 public class PhoneJoker implements Startable {
 
     private Set<Question.Answer> answerList;
+    private boolean alreadyUsed;
 
     public void setAnswerList(Set<Question.Answer> answerList) {
         this.answerList = answerList;
     }
+
+    private void usePhone() {
+        alreadyUsed = true;
+        Question.Answer rightAnswer = answerList
+                .stream()
+                .filter(Question.Answer::isRight)
+                .findFirst()
+                .get();
+
+        System.out.println("Hallo! Ich vermute die richtige Antwort lautet " + rightAnswer.answer());
+    }
     @Override
     public SelectedMenu.MenuSelection start() {
-        return null;
+        if (alreadyUsed) {
+            System.out.println("Telefon-Joker wurde schon benutzt.");
+            System.out.println("Bitte deine Antwort (a,b,c,d) oder <Joker> eingeben.");
+            return SelectedMenu.MenuSelection.BACK;
+        }
+        usePhone();
+        return SelectedMenu.MenuSelection.BACK;
     }
 }
