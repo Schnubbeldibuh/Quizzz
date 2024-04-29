33df89527e511146d5666eaf44618eb4ed42185a
Gloria <gloria.landsberger@gmail.com>
2024-04-29 19:07:24 +0200

    Tested to string and compare to of MP quick stats object

--- src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsMPQuickObjectTest.java
+++ src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsMPQuickObjectTest.java
@@ -2,8 +2,14 @@ package de.dhbw.ase.stats.persistance;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PlayerStatsMPQuickObjectTest {
 
@@ -17,6 +23,10 @@ class PlayerStatsMPQuickObjectTest {
 
     @Test
     void testToString() {
+        PlayerStatsMPQuickObject playerStatsMPQuickObject1 = new PlayerStatsMPQuickObject("test", 1, 2, 3);
+
+        String toString = playerStatsMPQuickObject1.toString();
+        Assertions.assertEquals("test;1;2;3", toString);
     }
 
     @Test
@@ -37,19 +47,25 @@ class PlayerStatsMPQuickObjectTest {
         Assertions.assertEquals(1, playerStatsMPQuickObject2.getFastestAnswer());
     }
 
-    @Test
-    void compareTo() {
+    @ParameterizedTest
+    @MethodSource("compareToValues")
+    void compareTo(int rightAnswer1, int rightAnswer2, int fastestAnswer1, int fastestAnswer2, int wrongAnswer1, int wrongAnswer2, int expected) {
+        PlayerStatsMPQuickObject playerStatsMPQuickObject1 = new PlayerStatsMPQuickObject("test", rightAnswer1, wrongAnswer1, fastestAnswer1 );
+        PlayerStatsMPQuickObject playerStatsMPQuickObject2 = new PlayerStatsMPQuickObject("test", rightAnswer2, wrongAnswer2, fastestAnswer2);
+
+        int i = playerStatsMPQuickObject1.compareTo(playerStatsMPQuickObject2);
+        Assertions.assertEquals(expected, i);
     }
 
-    @Test
-    void getRightAnswers() {
-    }
-
-    @Test
-    void getWrongAnswers() {
-    }
-
-    @Test
-    void getFastestAnswer() {
+    private static Stream<Arguments> compareToValues() {
+        return Stream.of(
+                arguments(1, 2, 2, 3, 1, 1, -1),
+                arguments(3, 2, 2, 3, 1, 1, 1),
+                arguments(2, 2, 2, 3, 1, 1, -1),
+                arguments(2, 2, 4, 3, 1, 1, 1),
+                arguments(2, 2, 3, 3, 1, 2, 1),
+                arguments(2, 2, 3, 3, 2, 1, -1),
+                arguments(2, 2, 4, 4, 1, 1, 0)
+        );
     }
 }
