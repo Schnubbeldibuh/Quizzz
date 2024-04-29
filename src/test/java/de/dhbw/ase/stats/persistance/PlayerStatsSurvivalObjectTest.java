0329f0fd37d870c7b7bf55e41154b5a9310aa2a1
Gloria <gloria.landsberger@gmail.com>
2024-04-29 19:40:30 +0200

    Tested to string and compare to of Survival stats object

--- src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsSurvivalObjectTest.java
+++ src/test/java/de/dhbw/ase/stats/persistance/PlayerStatsSurvivalObjectTest.java
@@ -2,8 +2,14 @@ package de.dhbw.ase.stats.persistance;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PlayerStatsSurvivalObjectTest {
 
@@ -51,29 +57,38 @@ class PlayerStatsSurvivalObjectTest {
 
     @Test
     void testToString() {
+        PlayerStatsSurvivalObject playerStatsSurvivalObject1 = new PlayerStatsSurvivalObject("test", 1, 2,3,4,5);
+
+        String toString = playerStatsSurvivalObject1.toString();
+        Assertions.assertEquals("test;1;2;3;4;5", toString);
     }
 
-    @Test
-    void compareTo() {
+    @ParameterizedTest
+    @MethodSource("compareToValues")
+    void compareTo(int rightAnswers1, int rightAnswers2,
+                   int wrongAnswers1, int wrongAnswers2,
+                   int averageDeathLevel1, int averageDeathLevel2,
+                   int totalSurvivedGames1, int totalSurvivedGames2,
+                   int totalPlayedGames1, int totalPlayedGames2,
+                   int expected) {
+        PlayerStatsSurvivalObject playerStatsSurvivalObject1 = new PlayerStatsSurvivalObject("test", rightAnswers1, wrongAnswers1, averageDeathLevel1, totalSurvivedGames1, totalPlayedGames1);
+        PlayerStatsSurvivalObject playerStatsSurvivalObject2 = new PlayerStatsSurvivalObject("test", rightAnswers2, wrongAnswers2, averageDeathLevel2, totalSurvivedGames2, totalPlayedGames2);
+
+        int i = playerStatsSurvivalObject1.compareTo(playerStatsSurvivalObject2);
+        Assertions.assertEquals(expected, i);
     }
 
-    @Test
-    void getRightAnswers() {
-    }
-
-    @Test
-    void getWrongAnswers() {
-    }
-
-    @Test
-    void getAverageDeathLevel() {
-    }
-
-    @Test
-    void getTotalSurvivedGames() {
-    }
-
-    @Test
-    void getTotalPlayedGames() {
+    private static Stream<Arguments> compareToValues() {
+        return Stream.of(
+                arguments(2, 3, 2, 3, 2, 2, 2, 2, 2, 3, -1),
+                arguments(3, 2, 2, 3, 2, 2, 2, 2, 2, 3, 1),
+                arguments(2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 1),
+                arguments(2, 2, 3, 2, 3, 3, 3, 3, 3, 3, -1),
+                arguments(2, 2, 3, 3, 4, 3, 2, 2, 2, 3, 1),
+                arguments(2, 2, 3, 3, 1, 2, 2, 2, 2, 3, -1),
+                arguments(2, 2, 3, 3, 4, 4, 4, 3, 2, 3, 1),
+                arguments(2, 2, 3, 3, 4, 4, 1, 2, 2, 3, -1),
+                arguments(2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 0)
+        );
     }
 }
