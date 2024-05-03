2345141ca7997c844ab79815f2eed85359e660e5
micha <mimagra@gmx.net>
2024-05-03 14:35:09 +0200

    improved SPQuizTest

--- src/test/java/de/dhbw/ase/play/games/singelplayer/SPQuizTest.java
+++ src/test/java/de/dhbw/ase/play/games/singelplayer/SPQuizTest.java
@@ -1,9 +1,7 @@
 package de.dhbw.ase.play.games.singelplayer;
 
 import de.dhbw.ase.SelectedMenu;
-import de.dhbw.ase.repository.QuestionRepository;
 import de.dhbw.ase.repository.QuestionRepositoryMock;
-import de.dhbw.ase.repository.StatsRepository;
 import de.dhbw.ase.repository.StatsRepositoryMock;
 import de.dhbw.ase.repository.question.Question;
 import de.dhbw.ase.stats.persistance.PlayerStatsSPObject;
@@ -19,6 +17,15 @@ class SPQuizTest {
     void spGameTestBlankStats() {
         UserInMock userInMock = new UserInMock();
         userInMock.inputs.offer("test");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("b");
+        userInMock.inputs.offer("b");
+        userInMock.inputs.offer("c");
+        userInMock.inputs.offer("d");
+        userInMock.inputs.offer("2");
 
         ArrayList<Question> questions = new ArrayList<>();
         while (questions.size() < 50) {
@@ -30,7 +37,7 @@ class SPQuizTest {
 
         StatsRepositoryMock statsRepositoryMock = new StatsRepositoryMock();
 
-        SPQuizMock spQuizTest = new SPQuizMock(userInMock, Categories.FOOD, questionRepositoryMock, statsRepositoryMock);
+        SPQuiz spQuizTest = new SPQuiz(userInMock, Categories.FOOD, questionRepositoryMock, statsRepositoryMock);
 
         SelectedMenu.MenuSelection start = spQuizTest.start();
         Assertions.assertEquals(SelectedMenu.MenuSelection.BACK, start);
@@ -47,6 +54,15 @@ class SPQuizTest {
     void spGameTestExistingStats() {
         UserInMock userInMock = new UserInMock();
         userInMock.inputs.offer("test");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("a");
+        userInMock.inputs.offer("b");
+        userInMock.inputs.offer("b");
+        userInMock.inputs.offer("c");
+        userInMock.inputs.offer("d");
+        userInMock.inputs.offer("2");
 
         ArrayList<Question> questions = new ArrayList<>();
         while (questions.size() < 50) {
@@ -59,7 +75,7 @@ class SPQuizTest {
         StatsRepositoryMock statsRepositoryMock = new StatsRepositoryMock();
         statsRepositoryMock.stats = List.of(new PlayerStatsSPObject("test", 0, 0, 0));
 
-        SPQuizMock spQuizTest = new SPQuizMock(userInMock, Categories.FOOD, questionRepositoryMock, statsRepositoryMock);
+        SPQuiz spQuizTest = new SPQuiz(userInMock, Categories.FOOD, questionRepositoryMock, statsRepositoryMock);
 
         SelectedMenu.MenuSelection start = spQuizTest.start();
         Assertions.assertEquals(SelectedMenu.MenuSelection.BACK, start);
@@ -89,51 +105,12 @@ class SPQuizTest {
 
         StatsRepositoryMock statsRepositoryMock = new StatsRepositoryMock();
 
-        SPQuizMock spQuizTest = new SPQuizMock(userInMock, Categories.FOOD, questionRepositoryMock, statsRepositoryMock);
+        SPQuiz spQuizTest = new SPQuiz(userInMock, Categories.FOOD, questionRepositoryMock, statsRepositoryMock);
 
         SelectedMenu.MenuSelection start = spQuizTest.start();
         Assertions.assertEquals(SelectedMenu.MenuSelection.EXIT, start);
 
         List<PlayerStatsSPObject> savedStats = statsRepositoryMock.savedStats;
         Assertions.assertEquals(0, savedStats.size());
-    }
-
-    static class SPQuizMock extends SPQuiz {
-        UserInMock sc;
-        int answeredQuestions;
-
-        public SPQuizMock(UserInMock sc, Categories category, QuestionRepository questionRepository, StatsRepository statsRepository) {
-            super(sc, category, questionRepository, statsRepository);
-            this.sc = sc;
-        }
-
-        @Override
-        protected List<Question.Answer> showQuestion(Question question) {
-            List<Question.Answer> answers = super.showQuestion(question);
-            Question.Answer rightAnswer = answers
-                    .stream()
-                    .filter(Question.Answer::isRight)
-                    .findFirst()
-                    .get();
-
-            int rightAnswerIndex = answers.indexOf(rightAnswer);
-            sc.inputs.offer(answeredQuestions < 4 ? intToAnswer(rightAnswerIndex) :
-                    rightAnswerIndex < 3 ? intToAnswer(++rightAnswerIndex) : intToAnswer(--rightAnswerIndex));
-            answeredQuestions++;
-            if (answeredQuestions == 8)
-                sc.inputs.offer("2");
-
-            return answers;
-        }
-
-        String intToAnswer(int i) {
-            return switch (i) {
-                case 0 -> "a";
-                case 1 -> "b";
-                case 2 -> "c";
-                case 3 -> "d";
-                default -> "";
-            };
-        }
     }
 }
---
+++ src/test/java/de/dhbw/ase/play/games/singelplayer/SingleplayerGame.java
@@ -0,0 +1,93 @@
+package de.dhbw.ase.play.games.singelplayer;
+
+import de.dhbw.ase.SelectedMenu;
+import de.dhbw.ase.play.games.ExitException;
+import de.dhbw.ase.play.games.Game;
+import de.dhbw.ase.repository.CouldNotAccessFileException;
+import de.dhbw.ase.repository.question.Question;
+import de.dhbw.ase.user.in.UserIn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SingleplayerGame extends Game {
+
+    private final UserIn sc;
+
+    public SingleplayerGame(UserIn sc) {
+        super(sc);
+        this.sc = sc;
+    }
+
+    protected abstract void startGame() throws ExitException, CouldNotAccessFileException;
+    protected abstract void writeStats() throws CouldNotAccessFileException;
+
+    @Override
+    public SelectedMenu.MenuSelection start() {
+        try {
+            indicateUser();
+        } catch (ExitException e) {
+            return SelectedMenu.MenuSelection.EXIT;
+        }
+
+        do {
+            try {
+                startGame();
+            } catch (ExitException e) {
+                return SelectedMenu.MenuSelection.EXIT;
+            } catch (CouldNotAccessFileException e) {
+                System.out.println("Das Spiel konnte nicht gestartet werden.");
+                System.out.println("Möglicherweise sind die Gamedaten kompromittiert");
+                return SelectedMenu.MenuSelection.BACK;
+            }
+            try {
+                writeStats();
+            } catch (CouldNotAccessFileException e) {
+                System.out.println("Beim speichern der Stats ist ein Fehler aufgetreten.");
+                System.out.println("Die Stats wurden nicht gespeichert.");
+            }
+        } while (askUserForRetry());
+        return SelectedMenu.MenuSelection.BACK;
+    }
+
+    protected boolean playQuestion(Question question) throws ExitException {
+        List<Question.Answer> answerList = showQuestion(question);
+        int answer = scanUntilValidInput();
+        return answerList.get(answer).isRight();
+    }
+
+    protected List<Question.Answer> showQuestion(Question question) {
+        List<Question.Answer> answerList = new ArrayList<>(question.getAnswerList());
+//        Collections.shuffle(answerList);
+        System.out.println();
+        System.out.println(question.getQuestion());
+        System.out.println("A: " + answerList.get(0).answer());
+        System.out.println("B: " + answerList.get(1).answer());
+        System.out.println("C: " + answerList.get(2).answer());
+        System.out.println("D: " + answerList.get(3).answer());
+        return answerList;
+    }
+
+    protected int scanUntilValidInput() throws ExitException {
+        int selection;
+        do {
+            String input = sc.waitForNextLine(this).toLowerCase();
+            if (input.equalsIgnoreCase("exit")) {
+                throw new ExitException();
+            }
+            if (input.length() == 1) {
+                selection = switch (input.charAt(0)) {
+                    case 'a' -> 0;
+                    case 'b' -> 1;
+                    case 'c' -> 2;
+                    case 'd' -> 3;
+                    default -> -1;
+                };
+                if (selection != -1)
+                    return selection;
+            }
+            System.out.println("Invalide Eingabe. Bitte erneut antworten.");
+        } while (true);
+    }
+
+}
